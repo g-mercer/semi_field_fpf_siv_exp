edfe2e1c0678afb131d083faca9f50ef7cda5d44
gmercer <UCFAGA0@ucl.ac.uk>
2023-03-29 14:52:36 +0100

    added bulk of semi-field analysis

--- mean_size_treatment_group/mean_workers_per_treatment.R
+++ mean_size_treatment_group/mean_workers_per_treatment.R
@@ -2,14 +2,16 @@ queen_df <- read.csv("~/projects/semi_field_fpf_siv_exp/mean_size_treatment_grou
 
 filtered_queens <- queen_df [!is.na(queen_df$number_of_workers_at_exposure_start), ]
 
-mean(filtered_queens$number_of_workers_at_exposure_start [filtered_queens$treatment_assigned == "control"])
+median(filtered_queens$number_of_workers_at_exposure_start [filtered_queens$treatment_assigned == "control"])
 
-mean(filtered_queens$number_of_workers_at_exposure_start [filtered_queens$treatment_assigned == "flup"])
+median(filtered_queens$number_of_workers_at_exposure_start [filtered_queens$treatment_assigned == "flup"])
 
-mean(filtered_queens$number_of_workers_at_exposure_start [filtered_queens$treatment_assigned == "sivanto"])
+median(filtered_queens$number_of_workers_at_exposure_start [filtered_queens$treatment_assigned == "sivanto"])
 
 sd(filtered_queens$number_of_workers_at_exposure_start [filtered_queens$treatment_assigned == "control"])
 
 sd(filtered_queens$number_of_workers_at_exposure_start [filtered_queens$treatment_assigned == "flup"])
 
 sd(filtered_queens$number_of_workers_at_exposure_start [filtered_queens$treatment_assigned == "sivanto"])
+
+median(filtered_queens$number_of_workers_at_exposure_start)
